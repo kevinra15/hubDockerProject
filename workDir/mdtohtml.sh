c74242137c8ea8cc514c3bfa49a4911abc938a79
Kevin Ramos Lopez <kevinramos456@gmail.com>
2016-04-04 12:14:33 +0200

    Script mdtohtml include template

--- workDir/mdtohtml.sh
+++ workDir/mdtohtml.sh
@@ -6,16 +6,19 @@
 #Script que converteix documents de markdown a html
 
 #Per cada file amb l'extensio .md
-for file in *.md;
+for file in *.md
 do
 	#Crea un file de format html
-	pandoc --from markdown \
-	--standalone  \
-	--output ${file%.md}.html \
-	$file
+	pandoc \
+		--standalone \
+		--from markdown \
+		--to html \
+		--template=plantillapandoc.tmpl \
+		--output ${file%.md}.html \
+		$file
 	
 	#Validem el doc generat
-	xmllint --noout --valid ${file}.html
+	xmllint --noout --valid ${file%.md}.html
 	#Comprovem si el doc generat es valid
 	codiErr=$?
 	if [[ $codiErr -ne 0 ]]
