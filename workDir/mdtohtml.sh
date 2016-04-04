18c66c001a9f481962edceff0dc1e0e758495df3
Kevin Ramos Lopez <kevinramos456@gmail.com>
2016-04-04 11:50:57 +0200

    New script mdtohtml

--- workDir/mdtohtml.sh
+++ workDir/mdtohtml.sh
@@ -13,5 +13,15 @@ do
 	--standalone  \
 	--output ${file%.md}.html \
 	$file
+	
+	#Validem el doc generat
+	xmllint --noout --valid ${file}.html
+	#Comprovem si el doc generat es valid
+	codiErr=$?
+	if [[ $codiErr -ne 0 ]]
+	then
+		echo "File generat ${file%.md}.html no valid!"
+		echo "Codi error: $codiErr"
+	fi
 done
 
